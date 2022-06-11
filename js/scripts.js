6b32e70da062318975cc9d7f86f06a8c56703269
Willian <williansilvap25@gmail.com>
2022-06-11 00:06:19 -0300

    bug fix js

--- js/scripts.js
+++ js/scripts.js
@@ -14,5 +14,6 @@ hamburgerMenu.addEventListener("click", () => { //quando há um clique
 document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", () => { // quando há um clique nos links da nav mobile
     hamburgerMenu.classList.remove("bi-x"); // remove o ícone x
     hamburgerMenu.classList.add("bi-list") // adiciona o ícone list
-    navMenu.classList.remove("active"); // remov a classe ativa da classe .navMenu
+    navMenu.classList.remove("active"); // remova a classe ativa da classe .navMenu
+    body.classList.toggle("menuNavActive"); //remove a classe no body
 }));
