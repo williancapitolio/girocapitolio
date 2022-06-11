548a02e8a14f83ec55efd8e75da67e37bf79f4f3
Willian <williansilvap25@gmail.com>
2022-06-10 23:55:33 -0300

    msg on js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 /* colocar um x quando clica nos itens da navbar */
 const hamburgerMenu = document.querySelector('.hamburgerMenu'); // pegando a classe .hamburgerMenu
-const body = document.querySelector('body'); // pegando a classe header
-const navMenu = document.querySelector('.navMenu');
+const body = document.querySelector('body'); // pegando a tag body
+const navMenu = document.querySelector('.navMenu'); // pegando a classe .navMenu
 
 hamburgerMenu.addEventListener("click", () => { //quando há um clique
     hamburgerMenu.classList.contains("bi-list") // se na classe .hamburgerMenu existir um bi-list
         ? hamburgerMenu.classList.replace("bi-list", "bi-x") //troque o bi-list por um bi-x
         : hamburgerMenu.classList.replace("bi-x", "bi-list"); //se não troque o bi-x por um bi-list
-    navMenu.classList.toggle("active");
-    body.classList.toggle("menuNavActive"); //adicionando a classe no header
+    navMenu.classList.toggle("active"); // adiciona a classe .navMenu e abre a navbar mobile
+    body.classList.toggle("menuNavActive"); //adicionando a classe no body
 });
 
-document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", () => {
-    hamburgerMenu.classList.remove("bi-x");
-    hamburgerMenu.classList.add("bi-list")
-    navMenu.classList.remove("active");
+document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", () => { // quando há um clique nos links da nav mobile
+    hamburgerMenu.classList.remove("bi-x"); // remove o ícone x
+    hamburgerMenu.classList.add("bi-list") // adiciona o ícone list
+    navMenu.classList.remove("active"); // remov a classe ativa da classe .navMenu
 }));
