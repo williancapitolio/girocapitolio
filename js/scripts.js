43594db10b8181d9cd1f1142018b7faedd9750c9
Willian <williansilvap25@gmail.com>
2022-06-12 00:23:36 -0300

    sec Passeios almost there

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,13 @@ hamburgerMenu.addEventListener("click", () => { //quando há um clique
         ? hamburgerMenu.classList.replace("bi-list", "bi-x") //troque o bi-list por um bi-x
         : hamburgerMenu.classList.replace("bi-x", "bi-list"); //se não troque o bi-x por um bi-list
     navMenu.classList.toggle("active"); // adiciona a classe .navMenu e abre a navbar mobile
-    body.classList.toggle("menuNavActive"); //adicionando a classe no body
+    //body.classList.toggle("menuNavActive"); //adicionando a classe no body
 });
 
 document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", () => { // quando há um clique nos links da nav mobile
     hamburgerMenu.classList.remove("bi-x"); // remove o ícone x
     hamburgerMenu.classList.add("bi-list") // adiciona o ícone list
     navMenu.classList.remove("active"); // remova a classe ativa da classe .navMenu
-    body.classList.toggle("menuNavActive"); //remove a classe no body
+    //if (document.querySelector("menuNavActive"))
+    //body.classList.toggle("menuNavActive"); //remove a classe no body
 }));
